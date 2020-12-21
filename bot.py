edb6b0b9191ab096d6d840856d4684a3c4adca79
David Bestz <75372386+Davoe-D@users.noreply.github.com>
2020-12-21 19:20:50 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,12 @@
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton('Bots Updates Channel', url='https://t.me/naijabestz.com')
+                ],
+                [
+                    InlineKeyboardButton('Support Group', url='https://t.me/naija_bestz')
+                ]
+            ]
+        )
