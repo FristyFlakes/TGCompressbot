0043684ab96bcd2d34aae33bcc1d725fd54315c8
David Bestz <75372386+Davoe-D@users.noreply.github.com>
2020-12-21 19:27:24 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,19 @@
+import datetime
+from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
-reply_markup=InlineKeyboardMarkup(
+@bot.on_message(filters.command('start') & filters.private)
+async def start(bot, message):
+    await message.reply(
+        text=f"**Hi {message.chat.first_name}!** \n\nThis is **Bestz URL Shorter Bot**. Just send me any big link and get short link.",
+        reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('Bots Updates Channel', url='https://t.me/naijabestz.com')
+                    InlineKeyboardButton('Bots Updates Channel', url='https://t.me/naijabestz')
                 ],
                 [
                     InlineKeyboardButton('Support Group', url='https://t.me/naija_bestz')
                 ]
             ]
         )
+    )
